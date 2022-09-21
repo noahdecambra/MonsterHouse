c828b244d17782211d05a0fce0635b306c163736
Oum-OW <jack.martin1@marist.edu>
2022-09-21 16:03:41 -0400

    Added Damage multiplier/resist

--- Monster House/Assets/Script/SprinklerLogic.cs
+++ Monster House/Assets/Script/SprinklerLogic.cs
@@ -1,37 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 public class SprinklerLogic : BuildableBase
 {
-    private int _damage= 3;
+    
+    private int _originalDamage = 3;
     private EnemyBase currentEnemyHealthManager;
     private int _superEffectiveMultiplier = 2;
     private float _resistedMultiplier = .5f;
     private string _superEffectiveType = "Unholy";
     private string _resistedType = "Magical";
     // Start is called before the first frame update
+    
     public override void Effect()
     {
+        int _damage=_originalDamage;
         currentEnemyHealthManager = enemyHealthManager;
         currentEnemyHealthManager.damageOverTime = true;
         currentEnemyHealthManager.giveTickDamage = true;
-        if (currentEnemyHealthManager.tag == _superEffectiveType)
+        if (currentEnemyHealthManager.gameObject.tag == _superEffectiveType)
         {
-            _damage *= _superEffectiveMultiplier;
+            _damage = _originalDamage * _superEffectiveMultiplier;
         }
 
-        if (currentEnemyHealthManager.tag == _resistedType)
+        else if (currentEnemyHealthManager.gameObject.tag == _resistedType)
         {
-            _damage *= (int)_resistedMultiplier;
+            _damage = (int)(_originalDamage * _resistedMultiplier);
         }
+
         currentEnemyHealthManager.DamageMonitor(_damage, 0, currentEnemyHealthManager.giveTickDamage);
-    }
-
-    void OnTriggerStay()
-    {
-        
     }
 
     void OnTriggerExit(Collider other)
